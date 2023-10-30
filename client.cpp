329e6339ceb3157746dec4651a2caec4bdb37bb5
Robert-Daniel Popa <134925940+DanielPopa03@users.noreply.github.com>
2023-10-30 18:09:34 +0200

    Create client.cpp

---
+++ client.cpp
@@ -0,0 +1,9 @@
+#include "../headere/client.h"
+
+std::ostream& operator<<(std::ostream& os, const Client& dt)
+    {
+    os << dt.nume << '/' << dt.adresa << '/' << dt.nrTelefon;
+    return os;
+    }
+
+Client::Client(const std::string& nume_,const std::string& adresa_) : nume{nume_},adresa{adresa_} {} 
