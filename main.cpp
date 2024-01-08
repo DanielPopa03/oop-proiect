124a86bb74f50248033289d7b6197458d62f6912
Robert-Daniel Popa <134925940+DanielPopa03@users.noreply.github.com>
2024-01-08 15:43:46 +0200

    gata

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 
 #include "headere/Pizzeria.h"
 
+#ifdef __linux__
+#include <X11/Xlib.h>
+#endif
 int main() {
+    #ifdef __linux__
+        XInitThreads();
+    #endif
     Pizzeria pizzeriaDorel;
     pizzeriaDorel.run();
 }
